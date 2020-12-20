89420ce62cae1036232ca4804c1bb22cb5a37742
Gustavo Oliveira <69910604+gugamenezes@users.noreply.github.com>
2020-12-20 00:13:49 -0300

    Version 1.0.0
    Version 1.0.0

--- youtubedownload.py
+++ youtubedownload.py
@@ -1,8 +1,19 @@
-import pytube
+import pytube 
 
-url = "https://www.youtube.com/watch?v=UM6YDJ2aalU&t=91s"
+url = "https://www.youtube.com/watch?v=UjTv8ivh7mY"
 
-video = pytube.Youtube(url)
+video = pytube.YouTube(url).streams
 
-for stream in video.streams:
-    print(stream)
+def downloadVideo(video):
+    video.filter(audio_codec="mp4a.40.2", type="video").first().download(filename="Video") 
+
+choose = input(f"Do you want to download *{url}* ?\n>").lower()
+
+if choose == "yes" or choose == "y":
+    print("Downloading...")
+    downloadVideo(video)
+    print("Download Complete!")
+elif choose == "no" or choose == "n":
+    print("Download Canceled!")
+else:
+    print("Invalid Operation!")
